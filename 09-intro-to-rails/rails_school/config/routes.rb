cc2ee499b1d0c5b4bd55ee32fd237c75b8d43bdd
Dwire <gjdwyer8@gmail.com>
2020-09-14 16:09:41 -0400

    afternoon

---
+++ 09-intro-to-rails/rails_school/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+                    
+                # Controoler#Action
+  get '/students', to: 'students#index', as: 'students' 
+  get '/students/:id', to: 'students#show', as: 'student' 
+
+end
